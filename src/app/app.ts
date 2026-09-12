a104eaf9802f6d9c944e76611ef319fbbd79a70f
IEVN1002-23000947 <ileana.cardiel@gmail.com>
2026-09-11 20:48:13 -0600

    heroes html 11/09/26

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,5 +7,17 @@ import { Component, signal } from '@angular/core';
   templateUrl: './app.html',
 })
 export class App {
-  protected readonly title = signal('introAngular');
-}
+  titulo:string="Ileana Cardiel"
+  //interpolacion: mandar a llamar la variable
+
+  duplicaNumero(num:number):number{
+    return num*2
+    }
+    pelicula={
+      titulo:"El padrino",
+      anio:1972,
+      genero:"Crimen/drama",
+      fechaLanzamiento: new Date(),
+      precio:345
+    }
+  }
